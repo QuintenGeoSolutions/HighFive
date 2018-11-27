7acdb34e315df14b758d3d222538f8c3419a6c29
ICONOS\vanasqu <quinten.vanassche@geosolutions.be>
2018-11-27 16:43:19 +0100

    no message

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/HighFive'));
+app.use(express.static(__dirname + '/dist/HighFiveRegister'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/HighFive/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/HighFiveRegister/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
